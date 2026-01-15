27484c6ceaba72fec5e39cb01f7ed17d2c631eb5
Adam Osmond <adam.osmond@garvin-allen.com>
2026-01-15 09:08:08 -0400

    application card context menu

--- client/src/components/CardApplication.js
+++ client/src/components/CardApplication.js
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import Card from '@mui/material/Card';
 import Typography  from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 function CardApplication ({application, onEdit, onDelete}){
+    const [contextMenuAnchor, setContextMenuAnchor] = useState(null);
+    const contextMenuOpen = Boolean(contextMenuAnchor);
+
+    const openContextMenu = (event) => {
+        setContextMenuAnchor(event.currentTarget);
+    };
+    const closeContextMenu = (action) => {
+        if(action){
+            action();
+        }
+        setContextMenuAnchor(null);
+    };
+
     const statusColorMap = {
         OPEN: 'application-status-open',
         REJECTED: 'application-status-rejected',
@@ -16,12 +33,34 @@ function CardApplication ({application, onEdit, onDelete}){
             <Typography variant="h6" component="div" className={`info application-card-header ${statusColorMap[application?.status ?? 'OPEN']}`}>
                 <span>{application?.company}</span>
                 <span className="application-card-status">
-                    <IconButton onClick={onEdit}>
-                        <EditIcon />
+                    <IconButton 
+                        id="basic-button"
+                        aria-controls={contextMenuOpen ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={contextMenuOpen ? 'true' : undefined}
+                        onClick={(e) => openContextMenu(e)}>
+                        <MoreVertIcon />
                     </IconButton>
-                    <IconButton onClick={onDelete}>
-                        <DeleteIcon />
-                    </IconButton>
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={contextMenuAnchor}
+                        open={contextMenuOpen}
+                        onClose={() => closeContextMenu()}
+                        slotProps={{
+                            list: {
+                                'aria-labelledby': 'basic-button',
+                            },
+                        }}
+                    >
+                        <MenuItem onClick={() => closeContextMenu(onEdit)}>
+                            <EditIcon sx={{mr: '1rem'}} />
+                            Edit
+                        </MenuItem>
+                        <MenuItem onClick={() => closeContextMenu(onDelete)}>
+                            <DeleteIcon sx={{mr: '1rem'}} />
+                            Delete
+                        </MenuItem>
+                    </Menu>
                 </span>
             </Typography>
             <Typography variant="body2" color="text.secondary" className="application-card-content">
